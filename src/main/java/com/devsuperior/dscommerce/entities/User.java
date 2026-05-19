219d11fd18d3fa34fb17110eeb479322f94cd7ef
alinasobral <alinasobral@hotmail.com>
2026-05-19 14:52:36 -0300

    GET user logged

--- src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -95,6 +95,10 @@ public class User implements UserDetails {
         return orders;
     }
 
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
     public void addRole(Role role) {
         roles.add(role);
     }
